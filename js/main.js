a24fea2f2b1b428130207523db6f0a48f8c94585
fieffee <estekhina2sub@mail.ru>
2015-11-08 23:12:54 +0300

    добавлен JS + мелкие исправления

---
+++ js/main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var link = document.querySelector(".btn-open-form");
+  var form = document.querySelector(".window-form");
+  var arrival = form.querySelector("[name=date-arrival]");
+  var departure = form.querySelector("[name=date-departure]")
+
+  link.addEventListener("click", function(event) {
+    event.preventDefault();
+    form.classList.toggle("window-form-open");
+    arrival.focus();
+  });
+
+  form.addEventListener("submit", function(event) {
+    if (!arrival.value || !departure.value) {
+       event.preventDefault();
+       console.log("Выберите интересующие Вас даты");
+       form.classList.add("window-form-error");
+       }
+  });
+
+});
